33f8d06b73cfd602bde33ee8b273bfb5bdb97b1b
Ofir160 <ofir1ziv2@gmail.com>
2025-07-31 18:53:23 +0100

    Fixed the strings

--- Scripts/thread.gd
+++ Scripts/thread.gd
@@ -1,29 +1,12 @@
 extends Line2D
 
-@export var NodeA : PhysicsBody2D
-@export var NodeB : PhysicsBody2D
-
-@onready var thread_segment: RigidBody2D = $ThreadSegment
-@onready var thread_segment_10: RigidBody2D = $ThreadSegment10
-
-@onready var pin_joint_2d: PinJoint2D = $PinJoint2D
-@onready var pin_joint_2d_11: PinJoint2D = $PinJoint2D11
-
-@export var physics_bodies : Array[RigidBody2D] = []
+@export var physics_bodies : Array[PhysicsBody2D] = []
 
 func _ready() -> void:
-	pin_joint_2d.node_a = NodeA.get_path()
-	pin_joint_2d_11.node_b = NodeB.get_path()
-	points[0].x = NodeA.global_position.x
-	points[0].y = NodeA.global_position.y
-	points[11].x = NodeB.global_position.x
-	points[11].y = NodeB.global_position.y
+	pass
 	
 func _physics_process(delta: float) -> void:
-	
-	print(physics_bodies[0].linear_velocity)
-	for i in range(0, 10):
+	for i in range(0, 11):
 		var body = physics_bodies[i]
-		points[i + 1].x = body.global_position.x + NodeA.position.x
-		#print(body.linear_velocity)
-		points[i + 1].y = body.global_position.y + NodeA.position.y
+		points[i].x = body.position.x 
+		points[i].y = body.position.y
