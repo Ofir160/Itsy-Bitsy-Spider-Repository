fdd6e80cae75b4f67779d1b977a540c10c5d8c33
Ofir160 <ofir1ziv2@gmail.com>
2025-07-31 22:21:26 +0100

    Finallized the strings
    its over anakin I have the high ground.
    You underestimated my power.
    (Epic fight scene)

--- Scripts/thread.gd
+++ Scripts/thread.gd
@@ -1,9 +1,21 @@
 extends Line2D
 
+@export var PointA : Node2D
+@export var PointB : Node2D
+
 @export var physics_bodies : Array[PhysicsBody2D] = []
 
 func _ready() -> void:
-	pass
+	position = PointA.position
+	var offset : Vector2 = PointB.position - PointA.position
+	var length : float = offset.length()
+	scale.y = length / 100
+	var angle : float
+	if PointA.position.y > PointB.position.y:
+		angle = PI - atan(offset.x / offset.y)
+	else:
+		angle = -atan(offset.x / offset.y)
+	rotation = angle
 	
 func _physics_process(delta: float) -> void:
 	for i in range(0, 11):
