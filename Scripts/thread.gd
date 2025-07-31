ecf7a5edbdef8a2d0190dd62fc8968baf6d852f6
Ofir160 <ofir1ziv2@gmail.com>
2025-07-31 18:29:32 +0100

    bad change

--- Scripts/thread.gd
+++ Scripts/thread.gd
@@ -9,21 +9,39 @@ extends Line2D
 @onready var pin_joint_2d: PinJoint2D = $PinJoint2D
 @onready var pin_joint_2d_11: PinJoint2D = $PinJoint2D11
 
-@export var physics_bodies : Array[RigidBody2D] = []
+@export var rigidbodies : Array[RigidBody2D] = []
+@export var joints : Array[PinJoint2D] = []
+
+var length : float
+var angle : float
 
 func _ready() -> void:
 	pin_joint_2d.node_a = NodeA.get_path()
 	pin_joint_2d_11.node_b = NodeB.get_path()
-	points[0].x = NodeA.global_position.x
-	points[0].y = NodeA.global_position.y
-	points[11].x = NodeB.global_position.x
-	points[11].y = NodeB.global_position.y
+	points[0].x = NodeA.position.x
+	points[0].y = NodeA.position.y
+	points[11].x = NodeB.position.x
+	points[11].y = NodeB.position.y
+	length = (NodeA.position - NodeB.position).length()
+	var offset : Vector2 = NodeB.position - NodeA.position
+	if NodeA.position.y > NodeB.position.y:
+		angle = 180 - atan(offset.x / offset.y) * 180.0 / PI
+	else:
+		angle = -atan(offset.x / offset.y) * 180.0 / PI
+	if angle < 0:
+		angle += 360
+	#print(angle)
+	for joint in joints:
+		joint.global_position += NodeA.position
+		print(joint.global_position)
+		#print(joint.global_position)
+	for rigidbody in rigidbodies:
+		rigidbody.global_position += NodeA.position
 	
 func _physics_process(delta: float) -> void:
-	
-	print(physics_bodies[0].linear_velocity)
 	for i in range(0, 10):
-		var body = physics_bodies[i]
-		points[i + 1].x = body.global_position.x + NodeA.position.x
-		#print(body.linear_velocity)
-		points[i + 1].y = body.global_position.y + NodeA.position.y
+		var body = rigidbodies[i]
+		points[i + 1].x = body.global_position.x
+		points[i + 1].y = body.global_position.y
+#func rotate_vector(input : Vector2, angle : float) -> Vector2:
+	
